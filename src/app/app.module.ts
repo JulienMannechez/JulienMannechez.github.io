223191c629e5d7e33d350856fe348cbc2051f5b5
Julien Mannechez <julien.mannechez@gmail.com>
2022-12-11 13:01:15 +0100

    experiences container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule, MatButtonModule, MatMenuModule, MatToolbarModule
+    MatIconModule, MatButtonModule, MatMenuModule, MatToolbarModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
